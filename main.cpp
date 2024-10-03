c6fd13b6d347963673aa4949faecc91d7d3203bd
MarlonPMan <Theforgottendreamer@protonmail.com>
2024-10-03 08:01:37 -0400

    dsd

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include <vulkan/vulkan.hpp>
+
+#include <d3d9.h>
+#include <d3d10.h>
+#include <d3d11.h>
+#include <d3d12.h>
+
+
+
+int main(int, char**){
+    std::cout << "Hello, from LibGPU!\n";
+}
---
+++ out/build/WindowsDebug/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Thefo/Projects/Code/C++/LibGPU
+# Build directory: C:/Users/Thefo/Projects/Code/C++/LibGPU/out/build/WindowsDebug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
