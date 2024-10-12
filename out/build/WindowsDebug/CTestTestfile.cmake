0215a1c2b7831b74a4e6215d96660a904f3aa252
Marlon Parks <72232895+TheforgottenDreamer@users.noreply.github.com>
2024-10-12 23:24:06 +0000

    remove build

--- out/build/WindowsDebug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Thefo/Projects/Code/C++/LibGPU
-# Build directory: C:/Users/Thefo/Projects/Code/C++/LibGPU/out/build/WindowsDebug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("ThirdParty/glfw")
-subdirs("ThirdParty/json")
-subdirs("ThirdParty/glm")
--- out/build/WindowsDebug/Resources/Shaders/TestShader/VK_TestShader.frag
+++
@@ -1,8 +0,0 @@
-#version 460
-
-layout(location=0) in vec3 FragColor;
-layout(location=1) in vec2 FragUV;
-
-void main() {
-    
-}
